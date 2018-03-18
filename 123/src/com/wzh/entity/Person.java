33682eacabafa11616c4c2a14480cabc83fc9019
wangzhihui <1301900713@qq.com>
2018-03-18 19:14:59 +0800

    添加属性

--- 123/src/com/wzh/entity/Person.java
+++ 123/src/com/wzh/entity/Person.java
@@ -2,4 +2,36 @@ package com.wzh.entity;
 
 public class Person {
 
+	private Integer id;
+	private String name;
+	private String gender;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("start");
+
+	}
 }
